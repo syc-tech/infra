666b6ae0d459ca9823b1a3d7d04b875d2401ce69
davidmnoll <davidmnoll@users.noreply.github.com>
2024-05-01 14:56:49 -0400

    add connection limit to db url

--- terraform/argo-apps/fos-web-backend.tf
+++ terraform/argo-apps/fos-web-backend.tf
@@ -94,7 +94,7 @@ resource "kubernetes_secret" "fos_db_credentials" {
     host     = data.digitalocean_database_cluster.postgres_cluster.host
     port     = tostring(data.digitalocean_database_cluster.postgres_cluster.port)
     dbname   = local.fos_db_name[terraform.workspace]
-    url      = "postgresql://${data.digitalocean_database_user.fos_web_user.name}:${data.digitalocean_database_user.fos_web_user.password}@${data.digitalocean_database_cluster.postgres_cluster.host}:${tostring(data.digitalocean_database_cluster.postgres_cluster.port)}/${local.fos_db_name[terraform.workspace]}"
+    url      = "postgresql://${data.digitalocean_database_user.fos_web_user.name}:${data.digitalocean_database_user.fos_web_user.password}@${data.digitalocean_database_cluster.postgres_cluster.host}:${tostring(data.digitalocean_database_cluster.postgres_cluster.port)}/${local.fos_db_name[terraform.workspace]}?connection_limit=5"
   }
 }
 
